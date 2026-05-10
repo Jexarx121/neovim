6e9d17cbd2ee5f825fa510938d15260fc7c8a2d3
jexarx <zjchen402@gmail.com>
2026-05-10 08:55:51 +0100

    Added nvim tree for file exploration

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            -- disable netrw so nvim-tree takes over
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+
+            require("nvim-tree").setup({
+                view = {
+                    width = 35,
+                    side = "left",
+                },
+                renderer = {
+                    group_empty = true,
+                    icons = {
+                        show = {
+                            file = true,
+                            folder = true,
+                            folder_arrow = true,
+                            git = true,
+                        },
+                    },
+                },
+                git = {
+                    enable = true,
+                    ignore = false,
+                },
+                filters = {
+                    dotfiles = false,
+                },
+                actions = {
+                    open_file = {
+                        quit_on_open = false,
+                    },
+                },
+            })
+
+            vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+            vim.keymap.set("n", "<leader>cd", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" })
+        end,
+    },
+}
